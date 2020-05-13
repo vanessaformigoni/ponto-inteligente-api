fa6ec1f926f2ed3f92e1072413454dc027a4a003
Vanessa Formigoni <vanessa.formigoni@hotmail.com>
2020-05-12 21:23:29 -0300

    adicionando controller funcionario

---
+++ src/main/java/com/vanessa/pontointeligente/api/dtos/FuncionarioDto.java
@@ -0,0 +1,92 @@
+package com.vanessa.pontointeligente.api.dtos;
+
+import java.util.Optional;
+
+import javax.validation.constraints.Email;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class FuncionarioDto {
+
+	private Long id;
+	private String nome;
+	private String email;
+	private Optional<String> senha = Optional.empty(); // vai criar logica se for passado altera, se não não
+	private Optional<String> valorHora = Optional.empty();
+	private Optional<String> qtdHorasTrabalhoDia = Optional.empty();
+	private Optional<String> qtdHorasAlmoco = Optional.empty();
+
+	public FuncionarioDto() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Length(min = 3, max = 200, message = "O nome não pode ser menor que 3 e nem maior que 200")
+	@NotEmpty(message = "O nome não pode ser vazio")
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Length(min = 3, max = 200, message = "O email não pode ser menor que 5 e nem maior que 200")
+	@NotEmpty(message = "O nome não pode ser vazio")
+	@Email(message = "Email invalido.")
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Optional<String> getSenha() {
+		return senha;
+	}
+
+	public void setSenha(Optional<String> senha) {
+		this.senha = senha;
+	}
+
+	public Optional<String> getValorHora() {
+		return valorHora;
+	}
+
+	public void setValorHora(Optional<String> valorHora) {
+		this.valorHora = valorHora;
+	}
+
+	public Optional<String> getQtdHorasTrabalhoDia() {
+		return qtdHorasTrabalhoDia;
+	}
+
+	public void setQtdHorasTrabalhoDia(Optional<String> qtdHorasTrabalhoDia) {
+		this.qtdHorasTrabalhoDia = qtdHorasTrabalhoDia;
+	}
+
+	public Optional<String> getQtdHorasAlmoco() {
+		return qtdHorasAlmoco;
+	}
+
+	public void setQtdHorasAlmoco(Optional<String> qtdHorasAlmoco) {
+		this.qtdHorasAlmoco = qtdHorasAlmoco;
+	}
+
+	@Override
+	public String toString() {
+		return "FuncionarioDto [id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + ", valorHora="
+				+ valorHora + ", qtdHorasTrabalhoDia=" + qtdHorasTrabalhoDia + ", qtdHorasAlmoco=" + qtdHorasAlmoco
+				+ "]";
+	}
+
+}
